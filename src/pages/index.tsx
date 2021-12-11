eb5641efce7f0812cdb3846cfd06d070abcb680c
daikimare <b1840702@planet.kanazawa-it.ac.jp>
2021-12-11 19:06:12 +0900

    create LP design

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,22 +5,22 @@ import topImage from '../assets/topImage.png'
 
 const Home: NextPage = () => {
   return (
-    <div>
-      <div className="flex left-44 text-red-600">
-        <h1>Serch Room</h1>
-        <p>金沢工業大学のレンタルスペースを予約できるアプリ</p>
-      </div>      
-    <div>
-        <button>登録する</button>
-        <button>ログインする</button>
-        <p>※金沢工業大学関係者のみ利用可能です。</p>
+    <div className=" justify-center mt-60 flex">
+      <div className="flex flex-col text-gray-900">
+        <h1 className=" text-6xl font-bold">Serch Room</h1>
+        <p className=" text-sm mt-4 font-bold">
+          金沢工業大学のレンタルスペースを予約できるアプリ
+        </p>
+        <div className="flex flex-col mt-16">
+          <button className=" text-sm font-semibold border-2 w-52 h-12 border-slate-800 rounded">
+            ログインする
+          </button>
+          <p className=" text-sm font-bold mt-7">
+            ※金沢工業大学関係者のみ利用可能です
+          </p>
+        </div>
       </div>
-      <Image
-        src={topImage}
-        alt="topImage"
-        width={586}
-        height={586}
-      />
+      <Image alt="topImage" height={417} src={topImage} width={586} />
     </div>
   )
 }
