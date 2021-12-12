5d633fdab961b993ece6bb7c5ab048298828356c
Tadao Iseki <me@saitolume.com>
2021-12-12 15:40:17 +0900

    ユーザーデータを保持する

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,21 +1,36 @@
-import { mdiAccount, mdiBellOutline, mdiCalendarBlankOutline } from '@mdi/js'
+import { mdiBellOutline, mdiCalendarBlankOutline } from '@mdi/js'
 import Icon from '@mdi/react'
+import Image from 'next/image'
+import { useUser } from '../hooks/useUser'
 import Button from './Button'
 
 const Header = () => {
+  const [user] = useUser()
+
   const checkButton = () => console.log('ボタンが押されました')
+
   return (
-    <header className="flex justify-between mx-36">
-      <h1 className="font-bold text-2xl mt-9">SearchRoom</h1>
-      <div className="flex mt-9">
+    <header className="flex justify-between items-center mx-36 pb-16 pt-4">
+      <h1 className="font-bold text-2xl">SearchRoom</h1>
+      <div className="flex items-center gap-4">
         <Button onClick={checkButton}>
           <div className="flex justify-center">
             <Icon path={mdiCalendarBlankOutline} size={1} />
             予約する
           </div>
         </Button>
-        <Icon className="ml-5 my-auto" path={mdiBellOutline} size={1} />
-        <Icon className="ml-5 my-auto" path={mdiAccount} size={1} />
+        <Icon className="" path={mdiBellOutline} size={1} />
+        {user?.photoURL && (
+          <button>
+            <Image
+              alt={`${user.displayName}のアイコン`}
+              className="rounded-full"
+              height={48}
+              src={user.photoURL}
+              width={48}
+            />
+          </button>
+        )}
       </div>
     </header>
   )
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,32 @@ import { onAuthStateChanged } from 'firebase/auth'
 import type { AppProps } from 'next/app'
 import { useEffect } from 'react'
 import { auth, signOut } from '../firebase/auth'
+import { UserContextProvider, useUser } from '../hooks/useUser'
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  useEffect(() => onAuthStateChanged(auth, console.log, signOut), [])
+function AppContainer(props: AppProps) {
+  return (
+    <UserContextProvider>
+      <App {...props} />
+    </UserContextProvider>
+  )
+}
+
+const App = ({ Component, pageProps }: AppProps) => {
+  const [, setUser] = useUser()
+  useEffect(
+    () =>
+      onAuthStateChanged(
+        auth,
+        (user) => {
+          setUser(user)
+        },
+        signOut
+      ),
+    []
+  )
 
   return <Component {...pageProps} />
 }
 
-export default MyApp
+export default AppContainer
