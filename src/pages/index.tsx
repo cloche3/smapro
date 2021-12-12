0bd4daa95bbf2b7becefdb8c50ad8827158485d1
clothe3 <henndoifu@gmail.com>
2021-12-12 13:13:48 +0900

    タイプの修繕

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
 import type { NextPage } from 'next'
+import Header from '../components/Header'
 import { signOut } from '../firebase/auth'
 
 const Page: NextPage = () => {
   return (
     <div>
+      <Header/>
       <button onClick={signOut}>ログアウト</button>
     </div>
   )
